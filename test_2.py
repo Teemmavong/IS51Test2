bdb3740fb455246ec73693c7d4ccd574b2afa44a
Teemmavong <70241711+Teemmavong@users.noreply.github.com>
2020-12-03 17:11:59 -0800

    Update test_2.py

---
+++ test_2.py
@@ -0,0 +1,12 @@
+"""
+We must read in a text file.
+Display the number of total grades in the text file
+Add all the grades into a listA. 
+Sum all the grades in the text file and divide by the total number of grades
+Close the the file
+Check to see if the grade in the listA is greater than the average, if so, add 
+to listB
+Divide sum of ListB by total number of grades.
+Print to user total number of grades, average grade, and percentage of grades 
+above the average.
+"""
